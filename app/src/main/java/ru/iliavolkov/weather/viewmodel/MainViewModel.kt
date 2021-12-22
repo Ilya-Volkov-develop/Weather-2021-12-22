9a01614e1cb88b0878b2c65e10c7300496a2da7e
Илья <ilia.volkov2016@mail.ru>
2021-12-22 19:04:16 +0300

    Урок 2 emulateRequest

--- app/src/main/java/ru/iliavolkov/weather/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/iliavolkov/weather/viewmodel/MainViewModel.kt
@@ -3,11 +3,19 @@ package ru.iliavolkov.weather.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
 
-class MainViewModel(val liveData: MutableLiveData<Any> = MutableLiveData()): ViewModel() {
+class MainViewModel(private val liveData: MutableLiveData<Any> = MutableLiveData()): ViewModel() {
 
     fun getLiveData():LiveData<Any>{
         return liveData
+    }
+
+    fun emulateRequest(){
+        Thread{
+            sleep(3000)
+            liveData.postValue(Any())
+        }.start()
     }
 
 }
