6c13ff82979d786a39607bb1c57096be631f7460
Илья <ilia.volkov2016@mail.ru>
2021-12-22 18:58:17 +0300

    Урок 2 Связали MainFragment.kt c MainViewModel

--- app/src/main/java/ru/iliavolkov/weather/view/MainFragment.kt
+++ app/src/main/java/ru/iliavolkov/weather/view/MainFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import ru.iliavolkov.weather.R
 import ru.iliavolkov.weather.databinding.FragmentMainBinding
+import ru.iliavolkov.weather.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
 
@@ -14,6 +16,13 @@ class MainFragment : Fragment() {
     private val binding:FragmentMainBinding
     get(){
         return _binding!!
+    }
+
+    lateinit var viewModel: MainViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
     override fun onDestroy() {
